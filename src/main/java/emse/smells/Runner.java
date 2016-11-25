3619723a462ce538d887adfbab71945c25673a7f
Maurício Aniche <mauricioaniche@gmail.com>
2016-11-25 08:34:28 +0100

    checking args

--- src/main/java/emse/smells/Runner.java
+++ src/main/java/emse/smells/Runner.java
@@ -6,6 +6,11 @@ public class Runner {
 
 	public static void main(String[] args) {
 		
+		if(args.length != 2) {
+			System.out.println("You should provide <project path> <output path>");
+			System.exit(-1);
+		}
+		
 		String projectPath = args[0];
 		String csvPath = args[1];
 		String pmdPath = System.getenv("EMSE_PMD_PATH");
